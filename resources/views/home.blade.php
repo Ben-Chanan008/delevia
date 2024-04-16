6dd9c96998f56385df0d97207b445f82d9e29883
Great Ben <bchanan.boss@gmail.com>
2024-04-16 17:33:35 -0600

    Final touches being added on the home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -188,8 +188,148 @@
             </div>
         </div>
     </section>
-    <section>
-
+    <section id="plans" class="mt-5">
+        <h2 class="text-center site-text-primary fw-bold">Delevia Pro</h2>
+        <p class="text-center">Go pro and unlock the full potential of <span class="site-text-primary">Delevia</span></p>
+        <div class="container mt-4">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="card h-100 p-5 border-0 bg-white shadow rounded-4">
+                        <p class="h2 text-center fw-bold site-text-primary">Delevia Elite</p>
+                        <div class="mt-3">
+                            <div class="mb-3">
+                                <i class="far fa-check text-success me-2"></i>
+                                <span class="">Lorem ipsum dolor sit amet.</span>
+                            </div>
+                            <div class="mb-3">
+                                <i class="far fa-check text-success me-2"></i>
+                                <span class="">Lorem ipsum dolor sit amet.</span>
+                            </div>
+                            <div class="mb-3">
+                                <i class="far fa-check text-success me-2"></i>
+                                <span class="">Lorem ipsum dolor sit amet.</span>
+                            </div>
+                            <div class="mb-3">
+                                <i class="far fa-check text-success me-2"></i>
+                                <span class="">Lorem ipsum dolor sit amet.</span>
+                            </div>
+                            <div class="mb-3">
+                                <i class="far fa-check text-success me-2"></i>
+                                <span class="">Lorem ipsum dolor sit amet.</span>
+                            </div>
+                            <div class="mb-3">
+                                <i class="far fa-check text-success me-2"></i>
+                                <span class="">Lorem ipsum dolor sit amet.</span>
+                            </div>
+                            <div class="mb-3">
+                                <i class="far fa-check text-success me-2"></i>
+                                <span class="">Lorem ipsum dolor sit amet.</span>
+                            </div>
+                            <div class="mb-3">
+                                <i class="far fa-check text-success me-2"></i>
+                                <span class="">Lorem ipsum dolor sit amet.</span>
+                            </div>
+                        </div>
+                        <div class="mt-3">
+                            <p class="site-text-primary text-center fs-2">$24.99/<span class="fs-6">month</span></p>
+                            <div class="d-flex">
+                                <button class="btn mx-auto btn-primary px-5">Try Now</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="card h-100 p-5 border-0 bg-white shadow rounded-4">
+                        <p class="h2 text-center fw-bold site-text-primary">Delevia Free</p>
+                        <div class="mt-3">
+                            <div class="mb-3">
+                                <i class="far fa-check text-success me-2"></i>
+                                <span class="">Lorem ipsum dolor sit amet.</span>
+                            </div>
+                            <div class="mb-3">
+                                <i class="far fa-check text-success me-2"></i>
+                                <span class="">Lorem ipsum dolor sit amet.</span>
+                            </div>
+                            <div class="mb-3">
+                                <i class="far fa-check text-success me-2"></i>
+                                <span class="">Lorem ipsum dolor sit amet.</span>
+                            </div>
+                            <div class="mb-3">
+                                <i class="far fa-check text-success me-2"></i>
+                                <span class="">Lorem ipsum dolor sit amet.</span>
+                            </div>
+                            <div class="mb-3">
+                                <i class="far fa-times text-danger me-2"></i>
+                                <span class="">Lorem ipsum dolor sit amet.</span>
+                            </div>
+                            <div class="mb-3">
+                                <i class="far fa-times text-danger me-2"></i>
+                                <span class="">Lorem ipsum dolor sit amet.</span>
+                            </div>
+                            <div class="mb-3">
+                                <i class="far fa-times text-danger me-2"></i>
+                                <span class="">Lorem ipsum dolor sit amet.</span>
+                            </div>
+                            <div class="mb-3">
+                                <i class="far fa-times text-danger me-2"></i>
+                                <span class="">Lorem ipsum dolor sit amet.</span>
+                            </div>
+                        </div>
+                        <div class="mt-3">
+                            <p class="site-text-primary text-center fs-2">Free/<span class="fs-6">month</span></p>
+                            <div class="d-flex">
+                                <button class="btn mx-auto btn-primary px-5">Try Now</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="card h-100 p-5 border-0 bg-white shadow rounded-4">
+                        <p class="h2 text-center fw-bold site-text-primary">Delevia Pro</p>
+                        <div class="mt-3">
+                            <div class="mb-3">
+                                <i class="far fa-check text-success me-2"></i>
+                                <span class="">Lorem ipsum dolor sit amet.</span>
+                            </div>
+                            <div class="mb-3">
+                                <i class="far fa-check text-success me-2"></i>
+                                <span class="">Lorem ipsum dolor sit amet.</span>
+                            </div>
+                            <div class="mb-3">
+                                <i class="far fa-check text-success me-2"></i>
+                                <span class="">Lorem ipsum dolor sit amet.</span>
+                            </div>
+                            <div class="mb-3">
+                                <i class="far fa-check text-success me-2"></i>
+                                <span class="">Lorem ipsum dolor sit amet.</span>
+                            </div>
+                            <div class="mb-3">
+                                <i class="far fa-check text-success me-2"></i>
+                                <span class="">Lorem ipsum dolor sit amet.</span>
+                            </div>
+                            <div class="mb-3">
+                                <i class="far fa-check text-success me-2"></i>
+                                <span class="">Lorem ipsum dolor sit amet.</span>
+                            </div>
+                            <div class="mb-3">
+                                <i class="far fa-times text-danger me-2"></i>
+                                <span class="">Lorem ipsum dolor sit amet.</span>
+                            </div>
+                            <div class="mb-3">
+                                <i class="far fa-times text-danger me-2"></i>
+                                <span class="">Lorem ipsum dolor sit amet.</span>
+                            </div>
+                        </div>
+                        <div class="mt-3">
+                            <p class="site-text-primary text-center fs-2">$19.99/<span class="fs-6">month</span></p>
+                            <div class="d-flex">
+                                <button class="btn mx-auto btn-primary px-5">Try Now</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
     @push('scripts')
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
