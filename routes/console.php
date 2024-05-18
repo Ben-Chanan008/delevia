f8bedc72e692790c5dd9c8bdc9517e80023d4532
Great Ben Ken-Idehen <bchanan.boss@gmail.com>
2024-05-18 11:03:33 -0600

    just playing around, artisan commands created

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,30 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('make:view {name} {--route=}', function(string $name) {
+    $route_config = $this->option('route') ? "resources/views/{$this->option('route')}/$name.blade.php" : "resources/views/$name.blade.php";
+    $route = $route_config;
+    $info = $this->option('route') ? "View created at resources/views/{$this->option('route')}/$name.blade.php" : "View created at resources/views/$name.blade.php";
+
+    $view_create = fopen($route, 'w') or die('Unable to open file!');
+    $view_appendables = '
+        <x-base>
+            <x-navbar />
+        </x-base/>
+    ';
+    fwrite($view_create, $view_appendables);
+    fclose($view_create);
+
+    $this->info($info);
+})->purpose('Create views for delevia');
+
+Artisan::command('delete:view {name} {--route=}', function(string $name) {
+    $route_config = $this->option('route') ? "resources/views/{$this->option('route')}/$name.blade.php" : "resources/views/$name.blade.php";
+    $route = $route_config;
+    $info = $this->option('route') ? "View deleted at resources/views/{$this->option('route')}/$name.blade.php" : "View deleted at resources/views/$name.blade.php";
+
+    unlink($route);
+
+    $this->info($info);
+})->purpose('Delete views for delevia');
